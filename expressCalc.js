6033130283ee1f0d131c0fe4ce23af12f51a73ea
Coomba <coombapace@gmail.com>
2019-05-01 19:47:48 -0400

    Calculator good to go

---
+++ expressCalc.js
@@ -0,0 +1,72 @@
+// Dependencies
+var express = require("express");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Create express app instance.
+var app = express();
+
+// Routes
+// What routes do you need to have? Which ones are optional?
+// TODO Add your routes here
+app.get("/:operator/:num1/:num2", function(req, res) {
+
+  // TODO parse out the variables from the request
+  // Parameters are received from the URL
+  // TODO make sure they're converted to integers (and not strings)
+  // Parameters are converted to integers
+  var operator = req.params.operator
+	var num1 = parseInt(req.params.num1);
+	var num2 = parseInt(req.params.num2);
+
+  // Initialize the result variable to send later
+  var result;
+  // Switch statement chooses operation based on the operation parameter.
+  switch (operator) {
+  // BONUS - How could you use * + etc. inside the app.get()?
+  case "add":
+    result = add(num1, num2);
+    break;
+  case "sub":
+    result = sub(num1, num2);
+    break;
+  case "multiply":
+    result = multiply(num1, num2);
+    break;
+  case "divide":
+    result = divide(num1, num2);
+    break;
+  default:
+    // Handle anything that isn't specified
+    result =
+        "Sorry! The only valid operations are add, sub, multiply, and divide.";
+  }
+
+  function add(a, b){
+    return a + b;
+  }
+
+  function sub(a, b){
+    return a - b;
+  }
+
+  function multiply(a, b){
+    return a * b;
+  }
+
+  function divide(a, b){
+    return a / b;
+  }
+
+  // We return the result back to the user in the form of a string
+  res.send(result.toString());
+
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
